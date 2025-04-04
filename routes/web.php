ff5233991a9b8e43b2fb784366a481c18f9107f7
Keerthishree13 <keerthishree@webtreeonline.com>
2025-04-04 12:27:30 +0530

    test

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
 
 Route::get('/categories/index',[CategoryController::class,'index'])->name('categories.index');
 Route::get('/categories/create',[CategoryController::class,'create'])->name('categories.create');
